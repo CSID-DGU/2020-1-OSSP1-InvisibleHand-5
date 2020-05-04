a4e93cb7b5f26f10a5f9b5e6c0b29a143d31e73a
Choiyunh_ <svs101@naver.com>
2020-05-04 21:01:40 +0900

    등장인물이 나오는 문장 판별 test

--- src/morphs.py
+++ src/morphs.py
@@ -4,12 +4,28 @@ from konlpy.tag import Komoran
 
 kom=Komoran(userdic='userDic.txt')
 
+listOfCharacter = ["김첨지","치삼이","개똥이"] # test
 
 f=open('../res/book/test1.txt', 'r', encoding='UTF8')
 
 for line in f:
     if len(line) > 1: # 추후 엔터 제거 전처리 적용 예정
         line = line.strip('\n')
-        print(kom.pos(line))
+        tokenList=kom.pos(line)
+        print(tokenList)
+        #print(len(tokenList))
+        """for i in tokenList:
+            for j in listOfCharacter:
+                if tokenList[i][0] == listOfCharacter[j]:"""
+        for i in tokenList:
+            if i[1] == 'NNP':  # 등장인물 선별 위해 고유명사 체크
+                tempList = i[0]
+        for i in tempList:
+            for j in listOfCharacter:
+                if i == j: # 등장인물이 있으면
+                    print(i)
+                    # 해당문장에 대한 감정분석
+
         print('\n')
 f.close()
+
