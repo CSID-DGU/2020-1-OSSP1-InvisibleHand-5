45d926cbdf7986b80a41070204ac2ef6dbc9e8d1
Choiyunh_ <svs101@naver.com>
2020-05-07 02:09:14 +0900

    형태소 분석 결과를 이용한 구문 분석 테스트(nltk)

--- src/morphs.py
+++ src/morphs.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
 
 from konlpy.tag import Komoran
+import nltk
 
-kom=Komoran(userdic='userDic.txt')
+kom=Komoran(userdic = 'userDic.txt')
 
-listOfCharacter = ["김첨지","치삼이","개똥이"] # test
+listOfCharacter = ["김첨지", "치삼이", "개똥이"] # test
+grammar = '''
+# 체언 = 명사/대명사/수사(NN*/NP/NR)
+# 주어 = 체언 + 주격 조사/보조사(JKS/JX)
+주어: {<N.*>+<JKS>}
+주어: {<N.*>+<JX>}
+# 서술어 = 동사/형용사(VV/VA) + 어미(E)
+#         체언 + 서술격조사(VCP/VCN) -> komoran에선 지정사 VC
+서술어: {<V.*><E.*>+}
+서술어: {<N.*>+<VC>}
+# 보어 = 체언 + 주격/보격 조사(JKS/JKC) + 되다/아니다
+#       -> 보격 조사가 대부분 주격 조사로 나오므로 주격 조사를 포함. 주어와 구분 필요
+보어: {<N.*>+<JKS>?<JX>?<VCN>}
+보어: {<N.*>+<JKS>?<JX>?<VV><EP><EC>}
+# 목적어 = 체언 + 목적격 조사/보조사(JKO/JX) + (부사) + 타동사
+#         -> 보조사가 붙을 때 주어와 구분짓기 위해 조건이 더 필요함
+목적어: {<N.*>+<JKO>}
+# 부사어 = 부사(MAG/MAJ)
+#         체언 + 부사격 조사(JKB)
+#         동사/형용사(VV/VA) + 부사형 어미(EC)
+#         -> komoran에 부사형 어미 존재X, 연결어미(EC)로 출력됨. 서술어와 구분 필요
+부사어: {<MA.*>}
+부사어: {<N.*>+<JKB>}
+부사어: {<V.*><EC>}
+# 관형어 = 관형사(MM)
+#         체언 + 관형격 조사(JKG)
+#         동사/형용사(VV/VA) + 관형사형 어미(ETM)
+관형어: {<MM>}
+관형어: {<N.*>+<JKG>}
+관형어: {<V.*><ETM>}
+# 독립어 = 감탄사(IC)
+#         체언 + 호격 조사(JKV)
+#         접속 부사(MAJ) -> 부사어와 구분이 필요 ex) '. 그리고', '. 그러나'
+독립어: {<IC>}
+독립어: {<N.*>+<JKV>}
+독립어: {<SF>+<MAJ>}
+'''
 
 f=open('../res/book/test1.txt', 'r', encoding='UTF8')
 
@@ -12,11 +49,24 @@ for line in f:
     if len(line) > 1: # 추후 엔터 제거 전처리 적용 예정
         line = line.strip('\n')
         tokenList=kom.pos(line)
-        print(tokenList)
-        #print(len(tokenList))
+
+        for token in tokenList:
+            if 'SW' in token[1]: #기타 기호 삭제
+                tokenList.remove(token)
+            if 'NA' in token[1]: # 분석불능범주 삭제
+                tokenList.remove(token)
+            if 'SH' in token[1]: # 한자 삭제
+                tokenList.remove(token)
+
+        words = tokenList
+        parser = nltk.RegexpParser(grammar)
+        chunks = parser.parse(words)
+
+        print(chunks.pprint())
+
         """for i in tokenList:
             for j in listOfCharacter:
-                if tokenList[i][0] == listOfCharacter[j]:"""
+                if tokenList[i][0] == listOfCharacter[j]:
         for i in tokenList:
             if i[1] == 'NNP':  # 등장인물 선별 위해 고유명사 체크
                 tempList = i[0]
@@ -25,6 +75,7 @@ for line in f:
                 if i == j: # 등장인물이 있으면
                     print(i)
                     # 해당문장에 대한 감정분석
+                    """
 
         print('\n')
 f.close()
