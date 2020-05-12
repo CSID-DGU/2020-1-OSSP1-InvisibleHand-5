f6c904b6836cc920e8281ee140181a55441b0ebc
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-05-12 23:29:05 +0900

    기능: 결과 그래프 출력 기능 모듈화

---
+++ src/result.py
@@ -0,0 +1,40 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import font_manager as fm
+
+def config_graph():
+    fontprop = fm.FontProperties(fname="../res/fonts/malgun.ttf", size=24).get_name()
+    plt.rc('font', family=fontprop)
+    plt.rcParams['figure.figsize'] = (10, 6)
+    plt.rcParams['axes.unicode_minus'] = False
+
+
+# 결과 1. 각 등장인물의 페이지별 감정 수준
+# 등장인물 별 그래프 생성 및 페이지별 감정 레벨 값 대입
+def display_emotion_graph(numOfCharacter, listOfCharacter, numOfPage, numOfEmotion, listOfEmotion, emotionVector):
+    x = np.arange(1, numOfPage)
+
+    for num in range(0, numOfCharacter):
+        plt.figure(num)
+        plt.title(f'{listOfCharacter[num]}')
+        plt.xlabel('페이지')
+        plt.ylabel('감정 레벨')
+        for emo in range(0, numOfEmotion):
+            plt.plot(x, emotionVector[num][emo][x], label=listOfEmotion[emo])
+
+        plt.legend()
+
+
+# 결과 2. 모든 등장인물의 페이지별 감정 흐름
+# 그래프 생성 및 등장인물 별 감정 흐름 값 대입
+def display_sentiment_graph(numOfCharacter, listOfCharacter, numOfPage, sentimentVector):
+    x = np.arange(1, numOfPage)
+
+    plt.figure(numOfCharacter)
+    plt.title("등장인물 별 감정 흐름")
+    plt.xlabel('페이지')
+    plt.ylabel('감정 레벨')
+    for num in range(0, numOfCharacter):
+        plt.plot(x, sentimentVector[num][x], label=listOfCharacter[num])
+
+    plt.legend()
