fee20541f77da510c703a681c2eae170a705c243
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-05-12 23:29:44 +0900

    기능: 전처리 모듈 파일명 변경 및 전처리 메소드 추가

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,14 +1,22 @@
 import re
+
+
 def del_new_lines(book):
     context = book.read().replace("\n", "")
     return context
 
-def change_to_qm(string): #매개변수 = 문자열 (꺽쇠를 쌍따옴표로)
+
+def change_to_qm(string):  # 매개변수 = 문자열 (꺽쇠를 쌍따옴표로)
     string = string.replace("「", "\"")
     string = string.replace("」", "\"")
     return string
 
-def remove_chinese(string): #매개변수 = 문자열.(한자 제거)
+
+def remove_chinese(string):  # 매개변수 = 문자열.(한자 제거)
     ch = '([\u4e00-\u9fff]+)'
     string = re.sub(ch, '', string)
     return string
+
+
+def remove_etc(book):
+    return remove_chinese(change_to_qm(del_new_lines(book)))
