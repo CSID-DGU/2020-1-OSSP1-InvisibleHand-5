aa343213c8e1c1a808055da089510cc22774c304
Choiyunh_ <svs101@naver.com>
2020-05-17 03:21:20 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,14 @@ import analyze
 import math
 import pandas as pd
 import morphs
+import noun_ex
 from matplotlib import pyplot as plt
 
+
+
 # 파일 입력
-fileName = input("소설명 : ")
+#fileName = input("소설명 : ")
+fileName = "운수좋은날"  # 테스트용 #########나중에 수정######################
 book = create.open_book(fileName)
 
 # 전처리
@@ -19,12 +23,16 @@ charOfPage = 700
 listOfEmotion = ['기쁨', '슬픔', '분노', '공포', '혐오', '놀람']
 numOfEmotion = len(listOfEmotion)
 numOfPage = math.ceil(len(context) / charOfPage)
-numOfCharacter = int(input("등장인물 수 : "))
+#numOfCharacter = int(input("등장인물 수 : "))
+numOfCharacter = 1 # 테스트용 #########나중에 수정######################
 listOfCharacter = []
 
 # 문장 테이블 생성
 df = create.create_sentence_table(context, listOfEmotion)
 create.save_df(df, fileName)
+
+# 명사 추출
+#noun_ex.noun_extract(df)
 
 # 사용자 사전 생성
 create.create_userdic(numOfCharacter, listOfCharacter)
@@ -47,11 +55,11 @@ analyzedEmotionVector = analyze.analyze_text(numOfPage, charOfPage, emotion_dict
 result.config_graph()
 
 # 결과 1. 각 등장인물의 페이지별 감정 수준 그래프 생성 및 출력
-result.display_emotion_graph(numOfCharacter, listOfCharacter, numOfPage, numOfEmotion, listOfEmotion,
-                             analyzedEmotionVector)
+#result.display_emotion_graph(numOfCharacter, listOfCharacter, numOfPage, numOfEmotion, listOfEmotion,
+              #               analyzedEmotionVector)
 
 # 결과 2. 모든 등장인물의 페이지별 감정 흐름 그래프 생성 및 출력
-result.display_sentiment_graph(numOfCharacter, listOfCharacter, numOfPage, sentimentVector)
+#result.display_sentiment_graph(numOfCharacter, listOfCharacter, numOfPage, sentimentVector)
 
 plt.show()
 book.close()
