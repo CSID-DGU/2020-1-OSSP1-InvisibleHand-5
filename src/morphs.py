8ce1caa0f40e7fac2a3cb87743abf67609383d53
Choiyunh_ <svs101@naver.com>
2020-05-17 18:21:03 +0900

    형태소 분석기 수정

--- src/morphs.py
+++ src/morphs.py
@@ -10,11 +10,17 @@ kom=Komoran(userdic = 'user_dic.txt') # 사용자 사전 적용
 # 형태소 분석
 def tokenizer(line):
     token_list = kom.pos(line)
-    for token in token_list:
+    for i, token in enumerate(token_list):
         if 'SW' in token[1]:  # 기타 기호 삭제
             token_list.remove(token)
         if 'NA' in token[1]:  # 분석불능범주 삭제
             token_list.remove(token)
         if 'SH' in token[1]:  # 한자 삭제
             token_list.remove(token)
+        if token[1] == 'NP' and token[0] == '그':
+            if token_list[i+1][0] == 'ㄴ' and token_list[i+1][1] == 'JX':
+                del token_list[i]
+                del token_list[i]
+                token_list.insert(i,('근','MM'))
+
     return token_list
