a5f339942d6184961e8716927ab6d45836d77273
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-06-19 05:54:26 +0900

    기능: 등장인물 주요 감정 및 감정 비율 출력 추가

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ import emotion_word
 import math
 import pandas as pd
 import noun_ex
-pd.set_option('mode.chained_assignment',  None)
 
+pd.set_option('mode.chained_assignment', None)
 
 # 파일 입력
-#fileName = input("소설명 : ")
+# fileName = input("소설명 : ")
 fileName = "운수좋은날"  # 테스트용 #########나중에 수정######################
 book = create.open_book(fileName)
 
@@ -20,12 +20,12 @@ context = preprocess.remove_etc(book)
 
 # 변수 선언
 # charOfPage = 700
-charOfPage = len(context)/20
+charOfPage = len(context) / 20
 listOfEmotion = ['기쁨', '슬픔', '분노', '공포', '혐오', '놀람']
 numOfEmotion = len(listOfEmotion)
 numOfPage = math.ceil(len(context) / charOfPage)
-#numOfCharacter = int(input("등장인물 수 : "))
-numOfCharacter = 1 # 테스트용 #########나중에 수정######################
+# numOfCharacter = int(input("등장인물 수 : "))
+numOfCharacter = 1  # 테스트용 #########나중에 수정######################
 listOfCharacter = []
 
 # 사용자 사전 생성
@@ -36,17 +36,15 @@ df = create.create_sentence_dataframe(context, listOfEmotion)
 create.save_df(df, fileName)
 
 # 명사 추출
-#noun_ex.noun_extract(df)
+# noun_ex.noun_extract(df)
 
-#emotion_word.emotion_lemmatization()
+# emotion_word.emotion_lemmatization()
 
 # 문장 데이터프레임 생성
 df_sentence = create.create_sentence_dataframe(context, listOfEmotion)
 
-
 # 감정 사전 생성
 emotion_dictionary_lists = emotion_word.create_emotion_dictionary()
-
 
 # 구축되어 있는 감정 사전 데이터 프레임 오픈
 df_emotion = emotion_word.open_emotion_dataframe()
@@ -55,17 +53,26 @@ df_emotion = emotion_word.open_emotion_dataframe()
 df_sentence = analyze.analyze_sentence(df_sentence, listOfCharacter, df_emotion, charOfPage)
 create.save_df(df_sentence, fileName)
 
-
 # 등장인물 별 페이지 감정 점수 합산하여 등장인물 데이터프레임 생성
 df_list_character = analyze.merge_character(df_sentence, listOfEmotion, listOfCharacter)
 df_list_character_by_page = analyze.merge_character_page(df_sentence, numOfPage, listOfEmotion, listOfCharacter)
 
 # 20 문장 당 x축 사이즈 = 1
 # 그래프 설정
-x_size = len(df_sentence.index)/20 + 1
+x_size = len(df_sentence.index) / 20 + 1
 result.config_graph(x_size)
 
 # 결과 1. 각 등장인물의 페이지별 감정 수준 그래프 생성 및 출력
-result.display_emotion_graph(df_list_character, df_list_character_by_page, listOfCharacter, numOfCharacter, listOfEmotion)
+result.display_emotion_graph(df_list_character, df_list_character_by_page, listOfCharacter, numOfCharacter,
+                             listOfEmotion)
 
+# 결과 3. 각 등장인물의 주요 감정
+emo_list = result.display_main_emo(df_list_character_by_page, numOfCharacter, listOfEmotion)
+for emo in emo_list:
+    print(emo)
+
+# 결과 4. 각 등장인물의 감정 비율
+ratio_list = result.display_emo_ratio(df_sentence, listOfCharacter, numOfCharacter, listOfEmotion)
+for num in range(0, numOfCharacter):
+    print(f'{listOfCharacter[num]}의 감정 비율 : {ratio_list[num]}')
 book.close()
--- src/result.py
+++ src/result.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import font_manager as fm
 
+
 def config_graph(x_size):
     fontprop = fm.FontProperties(fname="../res/fonts/malgun.ttf", size=24).get_name()
     plt.rc('font', family=fontprop)
@@ -13,7 +14,6 @@ def config_graph(x_size):
 # 결과 1. 각 등장인물의 페이지별 감정 수준
 # 등장인물 별 그래프 생성 및 페이지별 감정 레벨 값 대입
 def display_emotion_graph(df_list_character, df_list_character_by_page, listOfCharacter, numOfCharacter, listOfEmotion):
-
     for num in range(0, numOfCharacter):
 
         x = np.arange(0, len(df_list_character[num].index))
@@ -40,3 +40,48 @@ def display_emotion_graph(df_list_character, df_list_character_by_page, listOfCh
         plt.legend(loc='upper right')
         plt.grid(color='gray', dashes=(2, 2))
         plt.show()
+
+
+# 결과 2. 소설 장르, 시대적 배경 파악
+
+# 결과 3. 등장인물의 주요 감정 파악
+def display_main_emo(df_list_character_by_page, numOfCharacter, listOfEmotion):
+    main_emo_list = []
+    main_emo = ""
+    i = -1
+    for num in range(0, numOfCharacter):
+        df = df_list_character_by_page[num]
+        for emo in listOfEmotion:
+            if df[f'{emo}'].sum() > i:
+                main_emo = emo
+                i = df[f'{emo}'].sum()
+        main_emo_list.append(main_emo)
+        return main_emo_list
+
+
+def display_emo_ratio(df_sentence, listOfCharacter, numOfCharacter, listOfEmotion):
+
+    listOfRatio = []
+    # 화자 필터링
+    for character in listOfCharacter:
+        li = []
+        m1 = df_sentence['화자'] == character
+        filtered_df = df_sentence.loc[m1]
+        filtered_df = filtered_df.loc[:, ('기쁨', '슬픔', '분노', '공포', '혐오', '놀람')]  # 추출한 행들의 감정 열 추출
+        for emo in listOfEmotion:
+            count = 0
+            s_emo = filtered_df[emo]  # 시리즈 추출
+            for v in s_emo.values:
+                if v > 0:
+                    count = count + 1
+            li.append(count / len(filtered_df.index))  # 감정 비율 추가
+        count = 0
+        emo_s = filtered_df.sum(axis=1)  # 행 합의 시리즈
+        for s in emo_s:
+            if s == 0:
+                count = count + 1
+        li.append(count/len(filtered_df.index))
+        listOfRatio.append(li)  # 캐릭터 리스트에 추가
+    return listOfRatio
+
+
